4ea1b9fe8066be5db0d129eddd03667af43a9fc0
nallachaitu <nallachaitu@gmail.com>
2013-05-26 23:11:53 +0530

    Adding the definition of computeNumberOfWays class

---
+++ ComputeNumberOfWays.java
@@ -0,0 +1,12 @@
+/**
+	Problem: Given n numbers of dice with m number of faces where each face can have a value from 1 to m,
+	compute the number of ways possible for the sum of all the dice to a value x
+	m - no of faces
+	n - no of dice
+	x - given value
+*/
+
+public ComputeNumberOfWays
+{
+	
+}
