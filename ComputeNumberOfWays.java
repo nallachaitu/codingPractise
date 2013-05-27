627b180e1fc9a960ab9d0956fe932b146829de49
nallachaitu <nallachaitu@gmail.com>
2013-05-27 09:30:27 +0530

    adding the compute method definition and an initialize method

--- ComputeNumberOfWays.java
+++ ComputeNumberOfWays.java
@@ -8,5 +8,28 @@
 
 public ComputeNumberOfWays
 {
+	/**
+	 * Main function for computing the number of possible ways of throwing dice which 
+	 * would sum upto x.
+	 */
+	long computeWays(int m, int n, int x)
+	{
+		// using one additional row and column to start the numbering from 1
+		long[][] arr = new long[n+1][x+1];
+		
+		/**
+		 * since the number of ways for throwing dice given only dice is always one irrespective of the value. 
+		 */
+		intialize(arr,n,x);
+	}
+	
+	/**
+	 * It initializes the passed array by filling all the columns of the first row(with index 1) with 1.
+	 * 
+	 */
+	void intialize(long[][] arr,int n,int x)
+	{
+		
+	}
 	
 }
